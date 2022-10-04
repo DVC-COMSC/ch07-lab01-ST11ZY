397f361d00e7d7b0454d4ef4b041129bea339185
Your Name <you@example.com>
2022-10-04 12:37:42 -0700

    fixed code

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 # ******************************
 # Make your Code
 # ******************************
+list=[]
+minval=10000000000000
+maxval=-1
+totalInps=0
+totalSum=0
+while totalInps<5:
+    inp=int(input("Value?"))
+    totalSum=totalSum+inp
+    list.append(inp)
+    if list[totalInps]>maxval:
+        maxval=list[totalInps]
+    if list[totalInps]<minval:
+        minval=list[totalInps]
+    totalInps=totalInps+1
+totalSum=totalSum-maxval-minval
+print(totalSum)
